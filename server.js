9250642c18752438031ab1b8a6b1897a2d6140d8
Jason Huang <jasonhlx1213@gmail.com>
2020-10-11 22:03:09 +0300

    connected to database

--- server.js
+++ server.js
@@ -5,41 +5,26 @@
 // profile/:userId --> GET = user
 // image --> PUT --> user
 */
-const express = require('express');
+const express = require('express'); // express.js
 const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');  // for hash
 const cors = require('cors');
-// const bcrypt = require('bcrypt-nodejs');
+const knex = require('knex'); // for Postgres database
+const { response } = require('express');
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'postgres',
+    password : 'test',
+    database : 'smartbrain'
+  }
+});
 
 
 const app = express();
 
-const database = {
-  users: [
-    {
-      id: '123',
-      name: 'John',
-      password: 'cookies',
-      email: 'john@gmail.com',
-      entries: 0,
-      joined: new Date()
-    },
-    {
-      id: '124',
-      name: 'Sally',
-      password: 'bananas',
-      email: 'sally@gmail.com',
-      entries: 0,
-      joined: new Date()
-    }
-  ],
-  login: [
-    {
-      id: '987',
-      hash: '',
-      email: 'john@gmail.com'
-    }
-  ]
-}
 
 app.use(bodyParser.json());
 app.use(cors());
@@ -47,62 +32,95 @@ app.use(cors());
 
 // --> res = this is working
 app.get('/', (req, res)=> { // request, response
-  res.send(database.users);
+  res.send('success');
 })
 
 // signin --> POST = success/fail
+// Enable users to signin with correct
+// email and password stored in our postgresql
+// database.
 app.post('/signin', (req, res) => {
-  if (res.json(database.users[0])) {
-        res.json('success');
+  db.select('email', 'hash').from('login')
+    .where('email', '=', req.body.email)
+    .then(data => {
+      const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+      if (isValid) {
+        return db.select('*').from('users')
+          .where('email', '=', req.body.email)
+          .then(user => {
+            res.json(user[0]);
+          })
+          .catch(err => res.status(400).json('unable to get user'));
       } else {
-        res.status(400).json('error logging in');
+        res.status(400).json('wrong credentials')
       }
+    })
+    .catch(err => res.status(400).json('wrong credential'));
 })
 
 // register --> POST = user
+// add a new user into database
+// if the user already exists, error message
+// will be sent
 app.post('/register', (req, res) => {
   const { email, name, password } = req.body;
-  database.users.push({
-    id: '125',
-    name: name,
-    email: email,
-    entries: 0,
-    joined: new Date()
-  })
-  res.json(database.users[database.users.length-1]);
+  const hash = bcrypt.hashSync(password);
+    db.transaction(trx => {
+      trx.insert({
+        hash: hash,
+        email: email
+      })
+      .into('login')
+      .returning('email')
+      .then(loginemail => {
+        return trx('users')
+        .returning('*')
+        .insert({
+          email: loginemail[0],
+          name: name,
+          joined: new Date()
+        })
+        .then(user => {
+          res.json(user[0]);
+        })
+      })
+      .then(trx.commit)
+      .catch(trx.rollback)  // in case anything fails
+    })
+    .catch(err => res.status(400).json('unable to register'))
 })
 
 // profile/:userId --> GET = user
+// develop user profile page for each
+// user. It is not very useful now for this
+// application.
 app.get('/profile/:id', (req, res) => {
   const { id } = req.params;
-  let found = false;
-  database.users.forEach(user => {
-    if (user.id === id) {
-      found = true;
-      return res.json(user);
-    }
-  })
-  if (!found) {
-    res.status(400).json('no found');
-  }
+  db.select('*').from('users').where({id})
+    .then(user => {
+      console.log(user);
+      if (user.length) {
+        res.json(user[0]);
+      } else {
+        res.status(400).json('not found');
+      }
+    })
+    .catch(err => res.status(400).json('error getting user'))
 })
 
 // image --> PUT(update) --> user
+// Update user entries and increase
+// the counts.
 app.put('/image', (req, res) => {
   const { id } = req.body;
-  let found = false;
-  database.users.forEach(user => {
-    if (user.id === id) {
-      found = true;
-      user.entries++;
-      return res.json(user.entries);
-    }
+  db('users').where('id', '=', id)
+  .increment('entries',1)
+  .returning('entries')
+  .then(entries => {
+    res.json(entries[0]);
   })
-  if (!found) {
-    res.status(400).json('no found');
-  }
+  .catch(err => res.status(400).json('unable to get entries'))
 })
-
 
 
 app.listen(3001, () => {
