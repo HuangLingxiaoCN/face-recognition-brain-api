a377c547dcf63db3937c2b23a380cbb8013fbb69
Jason Huang <jasonhlx1213@gmail.com>
2020-10-16 23:16:18 +0300

    added email verification and secured clarifai api key

--- server.js
+++ server.js
@@ -1,10 +1,4 @@
-/*
-// --> res = database.users
-// signin --> POST = success/fail
-// register --> POST = user
-// profile/:userId --> GET = user
-// image --> PUT --> user
-*/
+
 const express = require('express'); // express.js
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');  // for hash
@@ -27,44 +21,22 @@ const db = knex({
   }
 });
 
-
 const app = express();
-
 
 app.use(bodyParser.json());
 app.use(cors());
-
 
 // --> response 'success'
 app.get('/', (req, res)=> { // request, response
   res.send('success');
 })
 
-// signin --> POST = success/fail
-// Enable users to signin with correct
-// email and password stored in our postgresql
-// database.
 app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt)});
-
-// register --> POST = user
-// add a new user into database
-// if the user already exists, error message
-// will be sent
 app.post('/register', (req, res) => { register.handleRegister(req,res,db,bcrypt)});
-
-// profile/:userId --> GET = user
-// develop user profile page for each
-// user. It is not very useful now for this
-// application.
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req,res,db)});
-
-// image --> PUT(update) --> user
-// Update user entries and increase
-// the counts.
 app.put('/image', (req,res) => { image.handleImage(req,res,db)});
-
+app.post('/imageurl', (req,res) => { image.handleApiCall(req,res)});
 
 app.listen(3001, () => {
   console.log('app is running on port 3001');
 })
-
